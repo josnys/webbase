8044176c193b485dda5a26a212f0edba4e5c6451
josnys <dirjos2014@gmail.com>
2023-03-29 02:35:18 -0400

    Upgrade to Laravel 10
    Upgrade the framework to version 10 and fixed a few bugs as well.

--- app/Http/Controllers/Admin/RoleController.php
+++ app/Http/Controllers/Admin/RoleController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\RolePermissionRequest;
 use Illuminate\Http\Request;
 use App\Http\Requests\RoleRequest;
 use App\Models\Role;
@@ -83,7 +84,9 @@ class RoleController extends Controller
           try {
                $role = $role->with('permissions')->find($role->id);
                $_permissions = Permission::all();
-               $role_permission = $role->permissions->pluck('id')->toArray();
+               $role_permission = $role->permissions->map(function($perm){
+                    return ['id' => $perm->id];
+               })->toArray();
 
                $permissions = array();
                foreach($_permissions as $permission){
@@ -91,7 +94,7 @@ class RoleController extends Controller
                          'id' => $permission->id,
                          'display' => $permission->display_name,
                          'description' => $permission->description,
-                         'isCheck' => in_array($permission->id, $role_permission)
+                         'isCheck' => (array_search($permission->id, array_column($role_permission, 'id')) === false) ? false : true
                     ]);
                }
 
@@ -111,14 +114,11 @@ class RoleController extends Controller
           }
      }
 
-     public function postAssign(Request $request, Role $role)
+     public function postAssign(RolePermissionRequest $request, Role $role)
      {
           try {
-               $input = $request->validate([
-                    'role_permissions' => ['required', 'array'],
-                    'role_permissions.*' => ['integer'],
-               ]);
-
+               $input = $request->validated();
+               
                $role->syncPermissions($input['role_permissions']);
 
                return redirect()->route('admin.security.index')->with('success', 'Permissions successfully assigned to '.$role->display_name);
--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -11,7 +11,7 @@ function Welcome() {
                </Head>
                <div className="flex items-center justify-center min-h-screen p-6">
                     <div className="w-full max-w-xl p-4 text-lg leading-loose text-center text-gray-500 bg-white rounded shadow-xl">
-                         Welcome to the <h1 className="text-xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-300 to-purple-500">Laravel 9 | Fortify | TailwindCSS 3 | InertiaJs | Reactjs</h1>
+                         Welcome to the <h1 className="text-xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-300 to-purple-500">Laravel 10 | Fortify | TailwindCSS 3 | InertiaJs | Reactjs</h1>
                     starter by <Link className="hover:text-purple-600" href="https://twitter.com/josnyS">JosnyS</Link><br />
                     <Link className="mr-2 text-xs hover:underline hover:text-blue-500" href={route('login')}>Login</Link>
                          <Link className="text-xs hover:underline hover:text-blue-500" href={route('register')}>Register</Link>
